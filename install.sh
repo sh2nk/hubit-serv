bb09c4f2ef4d0ecf3e5430a978b39734e719f5f6
sh2nk <nekkt.sh@gmail.com>
2019-10-03 21:31:30 +0300

    fixed install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 json=`curl -s "https://cloud-api.yandex.net:443/v1/disk/public/resources/download?public_key=https%3A%2F%2Fyadi.sk%2Fd%2F_k1RiNI7yeEF_g" | python3 -c "import sys, json; print(json.load(sys.stdin)['href'])"`
-wget $json -O model_1_nimish.tflearn.data-00000-of-00001
+wget $json -O model.tflearn.data-00000-of-00001
 python3 -m venv venv
 . venv/bin/activate
 pip install setuptools wheel numpy tensorflow==1.5 tflearn imutils opencv-python flask
